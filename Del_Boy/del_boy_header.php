a2dcafa303faf2a77c093c4f9464e6777b9b7c27
Laksana11 <laxsu1198@gmail.com>
2023-12-27 14:34:10 +0530

    final version

---
+++ Del_Boy/del_boy_header.php
@@ -0,0 +1,38 @@
+<!-- <?php
+if(isset($message)){
+   foreach($message as $message){
+      echo '
+      <div class="message">
+         <span>'.$message.'</span>
+         <i class="fas fa-times" onclick="this.parentElement.remove();"></i>
+      </div>
+      ';
+   }
+}
+?> -->
+
+
+<header class="header">
+
+   <div class="flex">
+
+      <a href="delivery.php" class="logo">Delivery Boy</a>
+
+   
+
+      <div class="icons">
+         <div id="menu-btn" class="fas fa-bars"></div>
+         <div id="user-btn" class="fas fa-user-circle">
+         <span class="user">
+            <?php echo $_SESSION['deliveryboy_name']; ?>
+            </span>
+         </div> 
+      </div>
+
+      <div class="account-box">
+         <a href="../logout.php" class="delete-btn">logout</a>
+      </div>
+
+   </div>
+
+</header>
